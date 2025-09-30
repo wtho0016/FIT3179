59bda05703f0678f9aa92df13b959712c7cd1826
wtho0016 <wtho0016@student.monash.edu>
2025-09-30 18:17:35 +0800

    Upload w9.js to reflect changes for Week 9 homework

---
+++ w9.js
@@ -0,0 +1,4 @@
+var vg_1 = "w9.vg.json";
+ vegaEmbed("#chart", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
