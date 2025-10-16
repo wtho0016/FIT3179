8430a8163a507e09348147b412a4192ad8428cd1
wtho0016 <wtho0016@student.monash.edu>
2025-10-16 16:38:12 +0800

    Update charts for critique 2

--- DV2/dv2.js
+++ DV2/dv2.js
@@ -6,6 +6,23 @@ var vg_1 = "mapdom.vg.json";
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
+var vg_accom = "accom.vg.json";
+ vegaEmbed("#chart_accom", vg_accom, {
+  actions: false,
+  background: "transparent"
+}).then(function(result) {
+ // Access the Vega view instance as result.view if needed
+}).catch(console.error);
+
+var vg_accom2 = "accom2.vg.json";
+ vegaEmbed("#chart_accom2", vg_accom2, {
+  actions: false,
+  background: "transparent"
+}).then(function(result) {
+ // Access the Vega view instance as result.view if needed
+}).catch(console.error);
+
+
 var vg_10 = "top10.vg.json";
  vegaEmbed("#chart_top10", vg_10, {
   actions: false,
@@ -13,3 +30,4 @@ var vg_10 = "top10.vg.json";
 }).then(function(result) {
  // Access the Vega view instance as result.view if needed
 }).catch(console.error);
+
